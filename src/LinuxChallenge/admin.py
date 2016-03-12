094d5ab911867df1257a55deafaec14d66b9dbdf
zenmaibane <koteicream.1024@gmail.com>
2016-03-12 20:36:29 +0900

    消された歴史

---
+++ src/LinuxChallenge/admin.py
@@ -0,0 +1,32 @@
+from django.contrib import admin
+from cms.models import User, Level, Question, Flag, Answer
+
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('name', 'password', 'point',)
+    list_display_links = ('name', 'point',)
+admin.site.register(User, UserAdmin)
+
+
+class LevelAdmin(admin.ModelAdmin):
+    list_display = ('level',)
+    list_display_links = ('level',)
+admin.site.register(Level, LevelAdmin)
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('level', 'title', 'sentence',)
+    list_display_links = ('level',)
+admin.site.register(Question, QuestionAdmin)
+
+
+class FlagAdmin(admin.ModelAdmin):
+    list_display = ('flag', 'point', 'question',)
+    list_display_links = ('flag', 'point', 'question',)
+admin.site.register(Flag, FlagAdmin)
+
+
+class AnswerAdmin(admin.ModelAdmin):
+    list_display = ('user', 'question', 'userAnswer', 'flag',)
+    list_display_links = ('user', 'question', 'userAnswer', 'flag',)
+admin.site.register(Answer, AnswerAdmin)
---
+++ src/LinuxChallenge/models.py
@@ -0,0 +1,56 @@
+from django.db import models
+
+
+class User(models.Model):
+    """ユーザ"""
+    name = models.CharField(max_length=31)
+    password = models.CharField('パスワード', max_length=127)
+    point = models.IntegerField('合計点')
+
+
+class Level(models.Model):
+    """問題のレベル"""
+    level = models.IntegerField('レベル', max_length=1)
+
+
+class Question(models.Model):
+    """問題"""
+    level = models.ForeignKey(Level, verbose_name='レベル')
+    title = models.CharField('title', max_length=50, unique=True)
+    sentence = models.TextField("問題文")
+
+
+class Flag(models.Model):
+    """正解のフラグ"""
+    flag = models.CharField('フラグ', max_length=255)
+    point = models.IntegerField('得点', max_length=3)
+    question = models.ForeignKey(Question, verbose_name='問題')
+
+
+class Answer(models.Model):
+    """ユーザの解答"""
+    user = models.ForeignKey(User, verbose_name='解答者')
+    question = models.ForeignKey(Question, verbose_name='問題')
+    userAnswer = models.CharField('解答', max_length=255)
+    flag = models.ForeignKey('Flag', blank=True, null=True)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
