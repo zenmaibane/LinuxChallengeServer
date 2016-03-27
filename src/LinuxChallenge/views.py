adb642084c245e95103e200baad6966a50767c5c
Gemba Shu <koteicream.1024@gmail.com>
2016-03-27 21:25:48 +0900

    ユーザの回答に応じてフラグの正誤，重複メッセージを表記

--- src/LinuxChallenge/views.py
+++ src/LinuxChallenge/views.py
@@ -3,9 +3,9 @@ from django.core.urlresolvers import reverse
 from django.views.generic import View, TemplateView, CreateView, DetailView
 from LinuxChallenge.models import User, Question, Flag, Level, Answer
 from LinuxChallenge.forms import SignUpForm, FlagForm
-from django.shortcuts import render, render_to_response
-from django.core.exceptions import ObjectDoesNotExist
-
+from django.shortcuts import render, render_to_response, redirect
+from django.contrib.messages import error, success
+from django.template import RequestContext
 
 
 class IndexView(TemplateView):
@@ -14,11 +14,6 @@ class IndexView(TemplateView):
 
 class RankingView(TemplateView):
     template_name = 'ranking.html'
-
-
-# class ChallengeView(ListView):
-#     template_name = 'challenge.html'
-#     model = Flag
 
 
 class ChallengeView(View):
@@ -48,18 +43,25 @@ class QuestionDetailView(DetailView):
     # 表示するモデルの種類を指定する．
     # ここでは，Questionの中でも一つを表示するのでQuestionを指定する．．
     model = Question
+
     # 表示するテンプレートはquestion.html．
     # ちなみに，template内ではobjectという変数に検索結果が与えられるらしい．
     # http://shinriyo.hateblo.jp/entry/2015/02/28/Django%E3%81%AEDetailView%E3%81%AE%E3%83%86%E3%83%B3%E3%83%97%E3%83%AC%E3%83%BC%E3%83%88
+    # def get(self, request, *args, **kwargs):
+    #     self.object = self.get_object()
+    #     context = self.get_context_data(object=self.object)
+    #     form = FlagForm(initial={"answer": "", "q_id": self.object.id})
+    #     return render_to_response(template_name='question.html',
+    #                               dictionary={"form": form, "question": self.object}, context=context)
 
     def get(self, request, *args, **kwargs):
+        print(request.path)
         self.object = self.get_object()
         context = self.get_context_data(object=self.object)
-        form = FlagForm(initial={"answer": "", "q_id": self.object.id})
+        form = FlagForm(initial={"q_id": self.object.id})
         return render_to_response(template_name='question.html',
-                                  dictionary={"form": form, "question":self.object}, context=context)
-        # object = Question.objects.get(id=pk)
-        # render(template_name, object)
+                                  dictionary={"form": form, "question": self.object},
+                                  context_instance=RequestContext(request))
 
 
 class AnswerView(View):
@@ -68,29 +70,24 @@ class AnswerView(View):
         if form.is_valid():
             user = request.user
             q_id = form.cleaned_data['q_id']
-            ques = Question.objects.get(id=q_id)
+            question = Question.objects.get(id=q_id)
             user_answer = form.cleaned_data['answer']
+            question_page = "/questions/" + str(q_id)
             try:
-                flag = Flag.objects.get(question=ques, correct_answer__exact=user_answer)
-                #flag = Flag.objects.get(Flag(question=ques),Flag(correct_answer=user_answer))
-                #flag = Flag.objects.filter(question=ques, correct_answer__exact=user_answer)
+                flag = Flag.objects.get(question=question, correct_answer__exact=user_answer)
             except Flag.DoesNotExist:
-                answer = Answer(user=user, question=ques, user_answer=user_answer, flag=None)
+                answer = Answer(user=user, question=question, user_answer=user_answer, flag=None)
                 answer.save()
-                is_correct = False
-                return render(request=request, template_name="question.html",
-                              dictionary={"is_correct": is_correct})
-
-            #回答の重複処理
-            if flag and Answer.objects.filter(user=user, question=ques, flag=flag).exists():
-                is_duplicate = True
-                return render(request=request, template_name="question.html",
-                              dictionary={"is_duplicate": is_duplicate})
-            answer = Answer(user=user, question=ques, user_answer=user_answer, flag=flag)
+                error(request, "That's incorrect.")
+                return redirect(question_page)
+            # 回答の重複処理
+            if flag and Answer.objects.filter(user=user, question=question, flag=flag).exists():
+                error(request, "The flag is already submitted.")
+                return redirect(question_page)
+            success(request, "Correct!")
+            answer = Answer(user=user, question=question, user_answer=user_answer, flag=flag)
             answer.save()
-            is_correct = True
-            return render(request=request, template_name="question.html",
-                              dictionary={"is_correct": is_correct})
+            return redirect(question_page)
 
 
 def login(request):
